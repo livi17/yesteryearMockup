48ad27b626651b35f8b4d538f7433b04745cca41
Michael LaRiviere <michael.lariviere@aceroinc.ca>
2015-04-01 15:44:04 -0400

    add edit button functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
+/********************************************************************
+ *                                                                  *
+ *   YESTERYEAR APP                                                 *
+ *    main.js - v 1.0                                               *
+ *                                                                  *
+ *   Copyright (c) 2014 Michael LaRiviere (leaffan1984@gmail.com)   *
+ *   Licensed under the MIT license                                 *
+ *                                                                  *
+ *******************************************************************/
 
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+	var age;
+	var currYear = new Date().getFullYear();
+	var currMonth = new Date().getMonth();
+	var birthYear = 1973;
+	var birthMonth = 5;
+
+	age = (currYear - birthYear);
+	if(birthMonth>currMonth){
+		age = age-1;
+	}
+
+	console.log("currYear: "+ currYear);
+	console.log("currMonth: "+ currMonth);
+	console.log("age: "+ age);
+
+	function functionButton(e){
+		console.log("e: "+ e);
+	}
+
+	function functionClick(i) {
+        "use strict";
+        return function () {
+                functionButton("edit" + i);
+        };
+    }
+
+    for (i = 1; i < 36; i++) {
+        var funcEl = document.getElementById("edit" + i);
+        funcEl.addEventListener("click", functionClick(i));
+    }
+
+
+}); // end of DOMContentLoaded
